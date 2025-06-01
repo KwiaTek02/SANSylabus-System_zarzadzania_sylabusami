407cbe27d42ad1921f419c458e31971e04a6c239
KwiaTek02 <matik775@gmail.com>
2025-06-02 01:10:59 +0200

    02.06.2025 01:10

---
+++ Frontend/Shared/SylabusHistoriaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Frontend.Shared
+{
+    public class SylabusHistoriaDto
+    {
+        public int Id { get; set; }
+        public int SylabusId { get; set; }
+        public DateOnly DataZmiany { get; set; }
+        public DateTime CzasZmiany { get; set; }
+        public int ZmienionyPrzez { get; set; }
+        public string? OpisZmiany { get; set; }
+        public string? WersjaWtedy { get; set; }
+    }
+}
