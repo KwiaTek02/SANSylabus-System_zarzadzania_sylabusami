bb0207bee6f06cc2d4bf35dcdd3fb3f821211797
KwiaTek02 <matik775@gmail.com>
2025-06-06 06:55:10 +0200

    06.06 6:54 - Dodano wstępną strone do edycji sylabusa

---
+++ Frontend/Shared/KoordynatorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Frontend.Shared
+{
+    public class KoordynatorDto
+    {
+        public int Id { get; set; }
+        public string ImieNazwisko { get; set; } = default!;
+        public string? Tytul { get; set; }
+    }
+}
