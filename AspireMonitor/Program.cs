4b2316219c58735d9b741b6ca6ea0877ae37678d
KwiaTek02 <matik775@gmail.com>
2025-07-06 23:37:46 +0200

    Usuniety komentarz w Program.cs

--- AspireMonitor/Program.cs
+++ AspireMonitor/Program.cs
@@ -5,7 +5,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var backend = builder.AddProject<Projects.SANSylabusApi>("backend");
 
 var frontend = builder.AddProject<Projects.Frontend>("frontend")
-    .WithReference(backend); // Zak³adaj¹c, ¿e frontend rozmawia z backendem
+    .WithReference(backend); 
 
 
 builder.Build().Run();
